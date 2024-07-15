540d9c7afa9fe27321d2d8a614ad31c18290a78d
kryphos <kryphos@proton.me>
2024-07-16 01:17:19 +0200

    .

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,8 +1,9 @@
-{ ... }: {
+{ pkgs, ... }: {
   users.users = {
     "luca" = {
       isNormalUser = true;
       home = "/home/luca";
+      shell = "${pkgs.zsh}/bin/zsh";
       password = "test";
       extraGroups = [ "wheel" "networkmanager" ];
     };
