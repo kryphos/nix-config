4e33fd972253c7146540540150711c544bde3a16
kryphos <kryphos@proton.me>
2024-07-16 01:21:42 +0200

    .

--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -1,12 +1,13 @@
 { pkgs, lib, isWSL, ... }: {
   config = lib.mkIf isWSL {
+    environment.systemPackages = with pkgs; [ ly hyprland ];
+
     programs.ly.enable = true;
 
     programs.hyprland = {
       enable = true;
       xwayland.enable = true;
     };
-
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
     xdg.portal.enable = true;
