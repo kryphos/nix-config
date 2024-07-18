6659488e84fc2391a38ddc3a00f0517655b469ae
kryphos <kryphos@proton.me>
2024-07-19 01:36:02 +0200

    .

--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -1,21 +1,19 @@
-{ pkgs, lib, isWSL, ... }: {
-  config = lib.mkIf (!isWSL) {
-    services.xserver.enable = true;
-    services.xserver.displayManager = {
-      sddm.enable = true;
-      sddm.wayland.enable = true;
-      defaultSession = "Hyprland";
-    };
-
-    programs.hyprland = {
-      enable = true;
-      xwayland.enable = true;
-    };
-    environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-    xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-    services.printing.enable = true;
+{ pkgs, ... }: {
+  services.xserver.enable = true;
+  services.xserver.displayManager = {
+    sddm.enable = true;
+    sddm.wayland.enable = true;
+    defaultSession = "Hyprland";
   };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  services.printing.enable = true;
 }
