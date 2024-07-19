d76107f3c6aa81e36d78f478ad2c77eb19556f30
kryphos <kryphos@proton.me>
2024-07-19 22:26:10 +0200

    .

--- modules/system/wayland.nix
+++ modules/system/wayland.nix
@@ -1,7 +1,26 @@
 { pkgs, ... }: {
   services.displayManager = {
-     enable = true;
-     defaultSession = "hyprland";
+    enable = true;
+    defaultSession = "hyprland";
+  };
+
+  systemd.services.lemurs = {
+    enable = true;
+    description = "Lemurs";
+    after = [
+      "systemd-user-sessions.service"
+      "plymouth-quit-wait.service"
+      "getty@tty2.service"
+    ];
+    unitConfig = {
+      Type = "idle";
+      StandardInput = "tty";
+      TTYPath = "/dev/tty2";
+      TTYReset = "yes";
+      TTYVHangup = "yes";
+    };
+    serviceConfig = { ExecStart = "/run/current-system/sw/bin/lemurs"; };
+    aliases = [ "display-manager.service" ];
   };
 
   programs.hyprland = {
